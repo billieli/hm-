1c2231024f213347c6bf54c6e362bea52aa36459
billieli <billie.li.7.outlook.com>
2022-09-06 11:20:21 +0800

    comment:初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  lintOnSave: false,
+  transpileDependencies: true,
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 7776,
+      host: 'localhost'
+    }
+  }
 })
